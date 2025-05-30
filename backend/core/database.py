32ec37052442f9ceb7a5ee2db53ee054fe219701
William Lam <williamntlam@gmail.com>
2025-05-29 21:50:35 -0400

    feat: established connection to mongodb database.

---
+++ backend/core/database.py
@@ -0,0 +1,18 @@
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+MONGODB_URI = f"mongodb+srv://{os.getenv("MONGODB_USERNAME")}:{os.getenv("MONGODB_PASSWORD")}@{os.getenv("MONGODB_CLUSTER")}.ixmzem2.mongodb.net/?retryWrites=true&w=majority&appName={os.getenv("MONGODB_CLUSTER")}"
+
+# Create a new client and connect to the server
+client = MongoClient(MONGODB_URI, server_api=ServerApi('1'))
+
+# Send a ping to confirm a successful connection
+try:
+    client.admin.command('ping')
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+except Exception as e:
+    print(e)
