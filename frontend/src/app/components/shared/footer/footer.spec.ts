34170baa4f9e1ed5163073be5b12c9dc97aeae60
William Lam <williamntlam@gmail.com>
2025-06-16 19:46:06 -0400

    feat: created simple header

--- frontend/src/app/components/shared/footer/footer.spec.ts
+++ frontend/src/app/components/shared/footer/footer.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { Header } from './header';
+import { Footer } from './footer';
 
-describe('Header', () => {
-  let component: Header;
-  let fixture: ComponentFixture<Header>;
+describe('Footer', () => {
+  let component: Footer;
+  let fixture: ComponentFixture<Footer>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [Header]
+      imports: [Footer]
     })
     .compileComponents();
 
-    fixture = TestBed.createComponent(Header);
+    fixture = TestBed.createComponent(Footer);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
